fb54e27f575f0b2829adc716e4696c6a87b86192
Domen Hribernik <domen.hribernik4@gmail.com>
2023-06-04 20:37:21 +0200

    NI-79 Created style for login register and home page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { UserContext } from "../userContext";
 import { Link } from "react-router-dom";
+import './Header.css'
 
 function Header(props) {
     const userContext = useContext(UserContext);
@@ -9,11 +10,16 @@ function Header(props) {
         return userContext.user.admin;
     };
 
+    function handleToggle() {
+        const navbarNav = document.getElementById("navbarNav");
+        navbarNav.classList.toggle("show");
+    }
+
     return (
         <header className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
                 <Link className="navbar-brand" to="/">{props.title}</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={handleToggle}>
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <nav className="collapse navbar-collapse" id="navbarNav">
