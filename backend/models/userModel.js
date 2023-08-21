1ca86778cc707ce4fb4d3e0948b2ab0318333d3d
mjorkk <merisa.mustajbasic@gmail.com>
2023-08-21 21:26:42 +0200

    NI-54 fixed bcrypt dependency, dockerised application

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var bcrypt = require("bcrypt");
+var bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 var Schema = mongoose.Schema;
 const { makeAccessToken, makeRefreshToken } = require("../config/jwtUtils.js");
